51835bff8ba3e1a8f7eae2238f3ea81194f1ea5c
stefanom13 <stefanomancuso13@gmail.com>
2022-03-28 22:17:25 +0200

    creata ed inserita lunghezza variabile testo

---
+++ index.php
@@ -0,0 +1,31 @@
+<!-- Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+
+<?php 
+
+
+$text = 'Inizia con la lettera “c” e finisce con la “o”, è duro e peloso. Cos’è? Il Cocco!';
+$lunghezzaTesto = strlen($text);
+?>
+
+
+Una parola da censurare viene passata dall’utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito 
+con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <h1><?php echo $text; ?></h1>
+   <h2> il testo è lungo :<?php echo $lunghezzaTesto; ?> </h2>
+</body>
+</html>
+
